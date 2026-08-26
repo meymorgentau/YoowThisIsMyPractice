5a987d2bc43363ff63c30a4f507cac56b6ca374a
Vitalina Grabcevich <grvitavi@gmail.com>
2026-08-26 22:11:09 +0300

    готовая анимация бега

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -5,15 +5,17 @@ public class DinoController : MonoBehaviour
     [Header("Настройки прыжка")]
     public float силаПрыжка = 10f;
 
-   private Rigidbody2D физика;
-private bool наЗемле = false;
-private GameOverManager менеджерОкончания;
+    private Rigidbody2D физика;
+    private bool наЗемле = false;
+    private GameOverManager менеджерОкончания;
+    private Animator аниматор;
 
     void Start()
-{
-    физика = GetComponent<Rigidbody2D>();
-    менеджерОкончания = FindObjectOfType<GameOverManager>();
-}
+    {
+        физика = GetComponent<Rigidbody2D>();
+        менеджерОкончания = FindObjectOfType<GameOverManager>();
+        аниматор = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -29,19 +31,23 @@ private GameOverManager менеджерОкончания;
         {
             физика.linearVelocity = new Vector2(0, силаПрыжка);
             наЗемле = false;
+
+            аниматор.SetBool("Бежит", false);
         }
     }
 
-   void OnCollisionEnter2D(Collision2D столкновение)
-{
-    if (столкновение.gameObject.name == "Ground")
+    void OnCollisionEnter2D(Collision2D столкновение)
     {
-        наЗемле = true;
-    }
+        if (столкновение.gameObject.name == "Ground")
+        {
+            наЗемле = true;
 
-    if (столкновение.gameObject.CompareTag("Cactus"))
-    {
-        менеджерОкончания.ПоказатьМеню();
+            аниматор.SetBool("Бежит", true);
+        }
+
+        if (столкновение.gameObject.CompareTag("Cactus"))
+        {
+            менеджерОкончания.ПоказатьМеню();
+        }
     }
-}
 }

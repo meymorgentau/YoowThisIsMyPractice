897e1996ce41efdc37d3e266eeac72c85d9d39c1
Vitalina Grabcevich <grvitavi@gmail.com>
2026-08-26 22:29:31 +0300

    маленькие корректировки

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -5,10 +5,16 @@ public class DinoController : MonoBehaviour
     [Header("Настройки прыжка")]
     public float силаПрыжка = 10f;
 
+    [Header("Настройки двойного прыжка")]
+    public float множительВторогоПрыжка = 0.5f;
+
     private Rigidbody2D физика;
     private bool наЗемле = false;
     private GameOverManager менеджерОкончания;
     private Animator аниматор;
+
+    private int количествоПрыжков = 0;
+    private int максимальноеКоличествоПрыжков = 2;
 
     void Start()
     {
@@ -27,11 +33,20 @@ public class DinoController : MonoBehaviour
 
     void ПопытатьсяПрыгнуть()
     {
-        if (наЗемле)
+        if (количествоПрыжков < максимальноеКоличествоПрыжков)
         {
-            физика.linearVelocity = new Vector2(0, силаПрыжка);
-            наЗемле = false;
+            float силаТекущегоПрыжка = силаПрыжка;
 
+            if (количествоПрыжков == 1)
+            {
+                силаТекущегоПрыжка = силаПрыжка * множительВторогоПрыжка;
+            }
+
+            физика.linearVelocity = new Vector2(0, силаТекущегоПрыжка);
+
+            количествоПрыжков++;
+
+            наЗемле = false;
             аниматор.SetBool("Бежит", false);
         }
     }
@@ -41,6 +56,7 @@ public class DinoController : MonoBehaviour
         if (столкновение.gameObject.name == "Ground")
         {
             наЗемле = true;
+            количествоПрыжков = 0;
 
             аниматор.SetBool("Бежит", true);
         }
